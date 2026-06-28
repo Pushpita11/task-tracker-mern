4534282c29d7953ae2b61c76677a9327c3592710
Pushpita Kundu <pushpitakundu11t@gmail.com>
2026-06-28 14:25:15 +0530

    Rename abc to taskRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import taskRoutes from "./routes/abc.js";
+import taskRoutes from "./routes/taskRoutes.js";
 dotenv.config();
 console.log(process.env.MONGO_URI);
 connectDB();
